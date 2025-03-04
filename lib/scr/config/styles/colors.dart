15259898a00731f394ba622e6531118204c16549
RinaZavr <katrinbardal@gmail.com>
2025-03-04 09:03:05 +0300

    feat: Added colors

--- lib/scr/config/styles/colors.dart
+++ lib/scr/config/styles/colors.dart
@@ -1,3 +1,23 @@
+import 'package:flutter/material.dart';
+
 abstract class AppColors {
   const AppColors._();
+
+  static const Color gainsboro = Color(0xffdeddde);
+  static const Color dimgray = Color(0xff6d6a6d);
+  static const Color raisinblack = Color(0xff221d22);
+  static const Color rajah = Color(0xffffbe5c);
+  static const Color onyx = Color(0xff383438);
+  static const Color arsenic = Color(0xff433f43);
+  static const Color bleudefrance = Color(0xff338bdc);
+  static const Color celestialblue = Color(0xff478cce);
+  static const Color popstar = Color(0xffc54363);
+  static const Color ruddybrown = Color(0xffb06628);
+  static const Color royalpurple = Color(0xff875ab3);
+  static const Color polishedpine = Color(0xff579e90);
+  static const Color raisinblacksecond = Color(0xff262626);
+  static const Color gray = Color(0xff7e7e7e);
+  static const Color white = Color(0xffffffff);
+  static const Color platinum = Color(0xffe5e5e5);
+  static const Color pictonblue = Color(0xff49a1f2);
 }
--- lib/scr/config/styles/extensions/theme_colors.dart
+++ lib/scr/config/styles/extensions/theme_colors.dart
@@ -1,31 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:status_tracker/scr/config/styles/colors.dart';
 
 @immutable
 class ThemeColors extends ThemeExtension<ThemeColors> {
   const ThemeColors._({
-    required this.accentColor,
-    required this.primaryColor,
+    required this.textColor,
+    required this.secondaryTextColor,
+    required this.backgroundColor,
+    required this.buttonColor,
+    required this.backgroungIconColor,
+    required this.lightBorderColor,
+    required this.darkBorderColor,
+    required this.remoteColor,
+    required this.diseaseColor,
+    required this.vacationColor,
+    required this.studyColor,
+    required this.otherColor,
   });
 
   const ThemeColors.light()
-      : accentColor = Colors.white,
-        primaryColor = Colors.black;
+      : textColor = AppColors.gainsboro,
+        secondaryTextColor = AppColors.dimgray,
+        backgroundColor = AppColors.raisinblack,
+        buttonColor = AppColors.rajah,
+        backgroungIconColor = AppColors.onyx,
+        lightBorderColor = AppColors.arsenic,
+        darkBorderColor = AppColors.bleudefrance,
+        remoteColor = AppColors.celestialblue,
+        diseaseColor = AppColors.popstar,
+        vacationColor = AppColors.ruddybrown,
+        studyColor = AppColors.royalpurple,
+        otherColor = AppColors.polishedpine;
 
   const ThemeColors.dark()
-      : accentColor = Colors.black,
-        primaryColor = Colors.white;
+      : textColor = AppColors.raisinblacksecond,
+        secondaryTextColor = AppColors.gray,
+        backgroundColor = AppColors.white,
+        buttonColor = AppColors.rajah,
+        backgroungIconColor = AppColors.white,
+        lightBorderColor = AppColors.platinum,
+        darkBorderColor = AppColors.pictonblue,
+        remoteColor = AppColors.celestialblue,
+        diseaseColor = AppColors.popstar,
+        vacationColor = AppColors.ruddybrown,
+        studyColor = AppColors.royalpurple,
+        otherColor = AppColors.polishedpine;
 
-  final Color accentColor;
-  final Color primaryColor;
+  final Color textColor;
+  final Color secondaryTextColor;
+  final Color backgroundColor;
+  final Color buttonColor;
+  final Color backgroungIconColor;
+  final Color lightBorderColor;
+  final Color darkBorderColor;
+  final Color remoteColor;
+  final Color diseaseColor;
+  final Color vacationColor;
+  final Color studyColor;
+  final Color otherColor;
 
   @override
   ThemeExtension<ThemeColors> copyWith({
-    Color? accentColor,
-    Color? primaryColor,
+    Color? textColor,
+    Color? secondaryTextColor,
+    Color? backgroundColor,
+    Color? buttonColor,
+    Color? backgroungIconColor,
+    Color? lightBorderColor,
+    Color? darkBorderColor,
+    Color? remoteColor,
+    Color? diseaseColor,
+    Color? vacationColor,
+    Color? studyColor,
+    Color? otherColor,
   }) {
     return ThemeColors._(
-      accentColor: accentColor ?? this.accentColor,
-      primaryColor: primaryColor ?? this.primaryColor,
+      textColor: textColor ?? this.textColor,
+      secondaryTextColor: secondaryTextColor ?? this.secondaryTextColor,
+      backgroundColor: backgroundColor ?? this.backgroundColor,
+      buttonColor: buttonColor ?? this.buttonColor,
+      backgroungIconColor: backgroungIconColor ?? this.backgroungIconColor,
+      lightBorderColor: lightBorderColor ?? this.lightBorderColor,
+      darkBorderColor: darkBorderColor ?? this.darkBorderColor,
+      remoteColor: remoteColor ?? this.remoteColor,
+      diseaseColor: diseaseColor ?? this.diseaseColor,
+      vacationColor: vacationColor ?? this.vacationColor,
+      studyColor: studyColor ?? this.studyColor,
+      otherColor: otherColor ?? this.otherColor,
     );
   }
 
@@ -39,14 +100,64 @@ class ThemeColors extends ThemeExtension<ThemeColors> {
     }
 
     return ThemeColors._(
-      accentColor: Color.lerp(
-        accentColor,
-        other.accentColor,
+      textColor: Color.lerp(
+        textColor,
+        other.textColor,
         t,
       )!,
-      primaryColor: Color.lerp(
-        primaryColor,
-        other.primaryColor,
+      secondaryTextColor: Color.lerp(
+        secondaryTextColor,
+        other.secondaryTextColor,
+        t,
+      )!,
+      backgroundColor: Color.lerp(
+        backgroundColor,
+        other.backgroundColor,
+        t,
+      )!,
+      buttonColor: Color.lerp(
+        buttonColor,
+        other.buttonColor,
+        t,
+      )!,
+      backgroungIconColor: Color.lerp(
+        backgroungIconColor,
+        other.backgroungIconColor,
+        t,
+      )!,
+      lightBorderColor: Color.lerp(
+        lightBorderColor,
+        other.lightBorderColor,
+        t,
+      )!,
+      darkBorderColor: Color.lerp(
+        darkBorderColor,
+        other.darkBorderColor,
+        t,
+      )!,
+      remoteColor: Color.lerp(
+        remoteColor,
+        other.remoteColor,
+        t,
+      )!,
+      diseaseColor: Color.lerp(
+        diseaseColor,
+        other.diseaseColor,
+        t,
+      )!,
+      vacationColor: Color.lerp(
+        vacationColor,
+        other.vacationColor,
+        t,
+      )!,
+      studyColor: Color.lerp(
+        studyColor,
+        other.studyColor,
+        t,
+      )!,
+      otherColor: Color.lerp(
+        otherColor,
+        other.otherColor,
         t,
       )!,
     );
