ae394428fa59bf3ef260f104f501d8937dfcc219
RinaZavr <katrinbardal@gmail.com>
2025-03-04 11:13:37 +0300

    feat: Added icons

---
+++ lib/scr/common/consts/icons.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+abstract class AppAssets {
+  static const IconData lightThemeIcon = CupertinoIcons.brightness;
+  static const IconData darkThemeIcon = CupertinoIcons.moon;
+  static const IconData recordsIcon = CupertinoIcons.collections;
+  static const IconData filterIcon = CupertinoIcons.calendar;
+  static const IconData sortIcon = CupertinoIcons.sort_down;
+  static const IconData editIcon = CupertinoIcons.pencil;
+  static const IconData closeIcon = CupertinoIcons.clear;
+  static const IconData remoteIcon = CupertinoIcons.home;
+  static const IconData diseaseIcon = Icons.medication;
+  static const IconData vacationIcon = CupertinoIcons.globe;
+  static const IconData studyIcon = Icons.school;
+  static const IconData otherIcon = CupertinoIcons.car_detailed;
+  static const IconData monthBackIcon = CupertinoIcons.arrow_left;
+  static const IconData monthForwardIcon = CupertinoIcons.arrow_right;
+  static const IconData monthReturnIcon = CupertinoIcons.arrow_uturn_left;
+  static const IconData addIcon = CupertinoIcons.add;
+}
